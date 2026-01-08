a7c00cf4d17aa0ee51d8cdc198c210a87351cd66
Wasim <wasim@example.com>
2026-01-08 16:13:49 +0530

    gf

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         {/* ✅ Add Manifest + PWA meta tags */}
         <link rel="manifest" href="/manifest.json" />
+         {/* FAVICON */}
+        <link rel="icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <meta name="theme-color" content="#1f2937" />
         <meta name="description" content="Chimney Solutions CRM Progressive Web App" />
